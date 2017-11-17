e230d8965beda6a0a3d84f83e7ebb3079d65f61f
wjdan94 <walharth@bu.edu>
2017-11-17 04:13:27 -0700

    added support for saving xlsx

--- run.sh
+++ run.sh
@@ -8,19 +8,25 @@
 START=$1
 END=$2
 SAVE_DIR_PATH="./"
-SAVE_DIR_NAME="cleaned_data"
+SAVE_DIR_NAME_RDATA="cleaned_data_Rdata"
+SAVE_DIR_NAME_XLSX="cleaned_data_xlsx"
 
-echo "Downloading all data between ${START} - ${END}"
-./scripts/download_all_data.sh $START $END
+#echo "Downloading all data between ${START} - ${END}"
+#./scripts/download_all_data.sh $START $END
 
 echo ""
 echo "Creating a directory to save clean data"
-eval "mkdir ${SAVE_DIR_NAME}"
-echo "Clean data will be saved in ./${SAVE_DIR_NAME}"
+eval "mkdir ${SAVE_DIR_NAME_XLSX}"
+eval "mkdir ${cleaned_data_Rdata}"
+echo "Clean data will be saved in ./${SAVE_DIR_NAME_XLSX} and ./${cleaned_data_Rdata}"
 echo ""
 
+cd ${SAVE_DIR_NAME_XLSX}
+eval "rm *" 
+cd ..
+
 echo "Running R Script to clean downloaded data"
-RUN_R="Rscript --vanilla ./cleaning_script.R ${START} ${END} ${SAVE_DIR_PATH} ${SAVE_DIR_NAME}"
+RUN_R="Rscript --vanilla ./cleaning_script.R ${START} ${END} ${SAVE_DIR_PATH} ${SAVE_DIR_NAME_RDATA} ${SAVE_DIR_NAME_XLSX}"
 echo "${RUN_R}"
 
 echo ""
